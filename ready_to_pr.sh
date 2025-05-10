4363f1575906d908652536c89ea5eb3ae538c535
KCN-judu <kcnjudu@gmail.com>
2025-05-10 12:31:05 +0800

    feat!: change development focus from data visualization to a general-purpose graphics library, and add PathCommand, Path, Stroke, and Fill based on this new direction

--- ready_to_pr.sh
+++ ready_to_pr.sh
@@ -2,7 +2,7 @@ moon fmt
 moon clean
 moon check
 moon info
-moon coverage clean
-moon test --enable-coverage
-moon coverage report -f summary > coverage_summary.txt
-moon coverage report -f html
+#moon coverage clean
+#moon test --enable-coverage
+#moon coverage report -f summary > coverage_summary.txt
+#moon coverage report -f html
